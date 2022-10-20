26273e3f71fdc8181898c2eb3d615f03a96f4f7b
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2022-10-21 01:37:40 +0300

    Add dto user class and mapper config

---
+++ src/main/java/com/telros/data/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.telros.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
+
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String secondName;
+    @NotNull
+    private String firstName;
+    private String patronymic;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+    @Email
+    private String email;
+    @Pattern(regexp = "\\d{10}")
+    private String phoneNumber;
+}

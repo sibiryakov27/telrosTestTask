472a2d73344e706cfd4b473e886f11c5d3066576
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2022-10-21 01:31:37 +0300

    Add entity class for table users

---
+++ src/main/java/com/telros/data/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package com.telros.data.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@Accessors(chain = true)
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}

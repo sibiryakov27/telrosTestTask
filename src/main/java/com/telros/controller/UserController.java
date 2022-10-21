caece86b0815acff08601b735ea2f2026044778b
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2022-10-22 00:21:34 +0300

    Add functionality to manipulate with user info

--- src/main/java/com/telros/controller/UserController.java
+++ src/main/java/com/telros/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.telros.controller;
 
+import com.telros.data.dto.UserContactInfo;
 import com.telros.data.dto.UserDTO;
+import com.telros.data.dto.UserDetailedInformationRequest;
 import com.telros.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Validated
 @RequestMapping("/telros")
 public class UserController {
 
@@ -21,5 +31,35 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         List<UserDTO> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<UserDTO> addNewUser(@Valid @RequestBody UserDTO user) {
+        UserDTO addedUser = userService.saveUser(user);
+        return ResponseEntity.ok(addedUser);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable Integer id) {
+        UserDTO deletedUser = userService.deleteUser(id);
+        return ResponseEntity.ok(deletedUser);
+    }
+
+    @PutMapping("/user/detailed-info/{id}")
+    public ResponseEntity<UserDTO> updateUserInfo(
+            @PathVariable Integer id,
+            @Valid @RequestBody UserDetailedInformationRequest userInfo
+            ) {
+        UserDTO updatedUser = userService.updateUserInfo(id, userInfo);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PutMapping("/user/contact-info/{id}")
+    public ResponseEntity<UserDTO> updateUserContacts(
+            @PathVariable Integer id,
+            @Valid @RequestBody UserContactInfo userContacts
+    ) {
+        UserDTO updatedUser = userService.updateUserContacts(id, userContacts);
+        return ResponseEntity.ok(updatedUser);
     }
 }
--- src/main/java/com/telros/service/UserService.java
+++ src/main/java/com/telros/service/UserService.java
@@ -1,13 +1,17 @@
 package com.telros.service;
 
+import com.telros.data.dto.UserContactInfo;
 import com.telros.data.dto.UserDTO;
+import com.telros.data.dto.UserDetailedInformationRequest;
 import com.telros.data.entity.UserEntity;
+import com.telros.exception.NotFoundException;
 import com.telros.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +26,46 @@ public class UserService {
         return userEntities.stream()
                 .map(entity -> modelMapper.map(entity, UserDTO.class))
                 .collect(Collectors.toList());
+    }
+
+    public UserDTO saveUser(UserDTO user) {
+        UserEntity userEntity = modelMapper.map(user, UserEntity.class);
+        userEntity = userRepository.save(userEntity);
+        return modelMapper.map(userEntity, UserDTO.class);
+    }
+
+    public UserDTO deleteUser(Integer id) {
+        Optional<UserEntity> userEntity = userRepository.findById(id);
+        if (userEntity.isEmpty()) {
+            throw new NotFoundException("User with such id cannot be found [id = " + id + "]");
+        }
+        userRepository.delete(userEntity.get());
+        return modelMapper.map(userEntity.get(), UserDTO.class);
+    }
+
+    public UserDTO updateUserInfo(Integer id, UserDetailedInformationRequest userInfo) {
+        Optional<UserEntity> userEntity = userRepository.findById(id);
+        if (userEntity.isEmpty()) {
+            throw new NotFoundException("User with such id cannot be found [id = " + id + "]");
+        }
+        UserEntity updatedUser = userEntity.get();
+        updatedUser.setFirstName(userInfo.getFirstName())
+                .setSecondName(userInfo.getSecondName())
+                .setPatronymic(userInfo.getPatronymic())
+                .setBirthDate(userInfo.getBirthDate());
+        updatedUser = userRepository.save(updatedUser);
+        return modelMapper.map(updatedUser, UserDTO.class);
+    }
+
+    public UserDTO updateUserContacts(Integer id, UserContactInfo userContacts) {
+        Optional<UserEntity> userEntity = userRepository.findById(id);
+        if (userEntity.isEmpty()) {
+            throw new NotFoundException("User with such id cannot be found [id = " + id + "]");
+        }
+        UserEntity updatedUser = userEntity.get();
+        updatedUser.setEmail(userContacts.getEmail())
+                .setPhoneNumber(userContacts.getPhoneNumber());
+        updatedUser = userRepository.save(updatedUser);
+        return modelMapper.map(updatedUser, UserDTO.class);
     }
 }
